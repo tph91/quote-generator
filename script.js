1879e98e8e4ffbc29593b43fa2cb021c64f07cca
Thomas Peterhammer <thommythedev@gmail.com>
2020-10-25 00:53:19 +0200

    added a loader

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn= document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let quotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = quotes[Math.floor(Math.random() *  quotes.length)];
     // Check if Author field is blank and replace it with 'Unknown'
@@ -23,7 +36,9 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 
@@ -33,6 +48,7 @@ If fetch request works, get Quote from API.
 If fetch request does not work, get quote from local Quotes (see: quote.js)
 */
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
